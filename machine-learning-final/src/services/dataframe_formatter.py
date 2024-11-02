6aeffe65850545768f533d9c8c67a732702ca9d7
Diogo Faria <diogofaria073@gmail.com>
2024-11-02 17:18:08 -0300

    initial commit

---
+++ machine-learning-final/src/services/dataframe_formatter.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def header_formatter(df: pd.DataFrame) -> pd.DataFrame:
+    # Update the column names, remove spaces replacing to underscore, remove /
+    # and () replacing to underscore and put all in lower case
+    df.columns = df.columns.str.replace(' ', '_').str.replace(
+        '/', '_').str.replace('(', '').str.replace(')', '').str.lower()
+
+    return df
+
+
+›
